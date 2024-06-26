278a4e06c1a20552c2a67cbd6c57d384b534faa9
Alan Prophett <alan.prophett@softwire.com>
2024-06-26 12:18:08 +0100

    pawn can move 2 on initial move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -7,16 +7,25 @@ export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
     }
-
     public getAvailableMoves(board: Board) {
         let moves = new Array()
         const square = board.findPiece(this);
         const row = square.row;
         const col = square.col;
 
-        moves.push(Square.at(row-1, col))
-        moves.push(Square.at(row+1, col))
-        
+        if (this.player === Player.BLACK) {
+            moves.push(Square.at(row - 1, col))
+            if(row === 6){
+                moves.push(Square.at(row - 2, col))
+            }
+        }
+        else {
+            moves.push(Square.at(row + 1, col))
+            if(row === 1){
+                moves.push(Square.at(row + 2, col))
+            }
+        }
+
         return moves;
     }
 }
