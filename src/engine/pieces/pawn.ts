ee83925112fed5f3704845cc0e70a272aa6e9fd6
Alan Prophett <alan.prophett@softwire.com>
2024-06-26 12:02:15 +0100

    Pawn move backwards and forwards 1

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,14 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = new Array()
+        const square = board.findPiece(this);
+        const row = square.row;
+        const col = square.col;
+
+        moves.push(Square.at(row-1, col))
+        moves.push(Square.at(row+1, col))
+        
+        return moves;
     }
 }
